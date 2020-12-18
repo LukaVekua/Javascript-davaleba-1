c20b94e8277d45e019f1eacc12202749a6307558
LukaVekua <luka.vekua@gau.edu.ge>
2020-12-18 13:51:48 +0400

    1

---
+++ Davaleba.js
@@ -0,0 +1,66 @@
+function daxra_N1(name="ragaca"){
+    document.write("<p>"+"<b>"+"<i>"+"<u>"+name+"<u>"+"<i>"+"<b>"+"<p>"); 
+}
+
+function mimateba_N2(a, b){
+    document.write(a + b);
+}
+
+function fonti_N3(){
+  var text = "Hello World!"
+  document.write("<br>"+text.fontsize(15)+"<br>")    
+}
+
+function naturaluri_ricxvebi_N11(){
+var numbers = [], i = 1, endInt = 10;
+while (i <= endInt) {
+  numbers.push(i);
+  i++;
+}
+document.write("<br>"+numbers+"<br>")
+}
+
+function axali_range_N12(n){
+var ricxvebi = [], i = 1, endInt = n;
+while (i < endInt) {
+  ricxvebi.push("<br>"+i+"<br>");
+  i++;
+}
+document.write("<p>"+ricxvebi+"<p>")
+}
+  
+function kide_axali_shualedi_N13(m,n){
+var naturaluri = [] , i = m , endInt = n ; 
+while (i < endInt) {
+  naturaluri.push("<br>"+i+"<br>");
+  i++;
+}
+document.write("<p>"+naturaluri+"<p>")
+}
+
+function saboloo_N14(m,n){
+var shualedi = [], i = m , endInt = n ;
+while (endInt < i) {
+  shualedi.push("<br>"+i+"<br>");
+  i--;
+} 
+while (i < endInt) {
+  shualedi.push("<br>"+i+"<br>");
+  i++;
+
+}
+document.write("<p>"+shualedi+"<p>");
+}
+
+
+function myFunction_N4_N10() {
+  var x = document.getElementById("myTable");
+  x.style.backgroundColor = "silver"
+  x.style.borderWidth = "10px"
+  x.style.width = "600px"
+  x.style.height = "600px"
+} 
+
+ 
+
+         
